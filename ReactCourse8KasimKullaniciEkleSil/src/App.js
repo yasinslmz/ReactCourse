4bab7c9eb99e651733700ab10e6e722309b66a21
Yasin Çağrı Solmaz <solmazyasin@hotmail.com>
2023-11-08 14:57:54 +0300

    KullaniciEkleSil
    Kullanıcı ekleyip silinebilen bir liste uygulaması yapıldı

---
+++ ReactCourse8KasimKullaniciEkleSil/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Eklesil from './eklesil';
+
+function App() {
+  return (
+    <div className="App row">
+
+      <div className='col-md-5'>
+
+      </div>
+      <Eklesil/>
+      <div className='col-md-4'>
+
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ ReactCourse8KasimKullaniciEkleSil/src/eklesil.js
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import "./App.css"; // Eklesil.css dosyasını içe aktarın veya stil kodunu buraya ekleyin
+
+
+function Eklesil() {
+  const [kullanicilar, setKullanicilar] = useState([]); // Kullanıcıları tutan dizi state'i
+  const [yeniAd, setYeniAd] = useState(''); // Yeni kullanıcı adı
+  const [yeniSoyad, setYeniSoyad] = useState(''); // Yeni kullanıcı soyadı
+
+  const handleAdChange = (e) => {
+    setYeniAd(e.target.value);
+  };
+
+  const handleSoyadChange = (e) => {
+    setYeniSoyad(e.target.value);
+  };
+
+  const handleGonderClick = () => {
+    // Yeni kullanıcıyı oluştur
+    const yeniKullanici = { ad: yeniAd, soyad: yeniSoyad };
+
+    // Kullanıcıları güncelle ve yeni kullanıcıyı ekleyin
+    setKullanicilar([...kullanicilar, yeniKullanici]);
+
+    // Yeni kullanıcı bilgilerini sıfırla
+    setYeniAd('');
+    setYeniSoyad('');
+  }
+
+  const handleSilClick = (index) => {
+    // Kullanıcıyı silmek için indeksi kullanarak yeni bir kullanıcılar dizisi oluşturun
+    const yeniKullanicilar = [...kullanicilar];
+    yeniKullanicilar.splice(index, 1);
+    setKullanicilar(yeniKullanicilar);
+  }
+
+  return (
+    <div className="mt-2 col-md-2 mt-5">
+      <div className="form-group">
+        <label htmlFor="ad" className="mb-2">Ad</label>
+        <input
+          type="text"
+          id="ad"
+          className="form-control"
+          value={yeniAd}
+          onChange={handleAdChange}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="soyad" className="mb-2 mt-2">Soyad</label>
+        <input
+         
+          type="text"
+          id="soyad"
+          className="form-control mb-3"
+          value={yeniSoyad}
+          onChange={handleSoyadChange}
+        />
+      </div>
+      <button className="btn btn-success" onClick={handleGonderClick}>
+        Ekle
+      </button>
+
+      <h2>Kullanıcılar</h2>
+      <ul className="list-group">
+        {kullanicilar.map((kullanici, index) => (
+          <li key={index} className="list-group-item">
+            {kullanici.ad} {kullanici.soyad}
+            <button
+              className="btn btn-danger float-right ms-2"
+              onClick={() => handleSilClick(index)}
+            >
+              Sil
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Eklesil;
